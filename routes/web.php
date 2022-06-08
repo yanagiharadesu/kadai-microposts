66acae0e6d7511e342e0ada563d5c085c33c26cc
kaisei-yanagihara <kaisei-yanagihara@dac.co.jp>
2022-06-08 07:38:59 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('users', 'UserController', ['only' => ['index', 'show']
+]);
+});
+
 /*
 Route::get('/', function () {
     return view('welcome');
